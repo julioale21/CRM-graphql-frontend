9e65204ea9f13a3d4a726450ca52b79e9cefb19c
Julio Romero <julio_ale21@hotmail.com>
2021-03-18 23:44:20 -0300

    style: added styles to product list

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -7,13 +7,42 @@ class ProductsList extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Product List</h1>
+        <h2 className="text-center uppercase font-bold text-2xl my-4">
+          Product List
+        </h2>
 
-        <Query>
+        <Query query={GET_ALL_PRODUCTS_QUERY} pollInterval={1000}>
           {({ loading, error, data, startPolling, stopPollong }) => {
             if (loading) return "Cargando ...";
             if (error) return `Error: ${error.message}`;
-            return <h2>fasd</h2>;
+            return (
+              <table className="min-w-max w-full table-auto">
+                <thead>
+                  <tr className="bg-gray-200 text-gray-600 uppercase text-lg leading-normal">
+                    <th className="w-2/6 py-3 px-6 text-center">Name</th>
+                    <th className="w-1/6 py-3 px-6 text-center">Price</th>
+                    <th className="w-1/6 py-3 px-6 text-center">Stock</th>
+                    <th className="w-1/6 py-3 px-6 text-center">Delete</th>
+                    <th className="w-1/6 py-3 px-6 text-center">Edit</th>
+                  </tr>
+                </thead>
+                <tbody class="text-gray-600 text-sm font-light">
+                  {data.getProducts.map((product) => (
+                    <tr class="border-b border-gray-200 hover:bg-gray-100">
+                      <td class="py-3 px-6 text-center whitespace-nowrap">
+                        {product.name}
+                      </td>
+                      <td class="py-3 px-6 text-center whitespace-nowrap">
+                        {product.price}
+                      </td>
+                      <td class="py-3 px-6 text-center whitespace-nowrap">
+                        {product.stock}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            );
           }}
         </Query>
       </Fragment>
