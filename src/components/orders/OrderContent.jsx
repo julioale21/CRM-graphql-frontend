a3deb751fb5a49feb3237fd5f5b9fb9ff7aca78e
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-03-23 13:21:53 -0300

    chore: added Summary component to order content

--- src/components/orders/OrderContent.jsx
+++ src/components/orders/OrderContent.jsx
@@ -1,15 +1,24 @@
 import React, { Component, Fragment } from "react";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
+import Summary from "./Summary";
 
 class OrderContent extends Component {
-  state = {};
+  state = {
+    products: [],
+  };
+
+  selectProduct = (products) => {
+    this.setState({ products });
+  };
+
   render() {
     const animatedComponents = makeAnimated();
     return (
       <Fragment>
         <h2 className="text-center my-5 uppercase font-bold text-2xl">Order</h2>
         <Select
+          onChange={this.selectProduct}
           options={this.props.products}
           isMulti={true}
           components={animatedComponents}
@@ -17,6 +26,7 @@ class OrderContent extends Component {
           getOptionValue={(option) => option.id}
           getOptionLabel={(option) => option.name}
         />
+        <Summary products={this.state.products} />
       </Fragment>
     );
   }
---
+++ src/components/orders/Summary.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const Summary = (props) => {
+  const products = props.products;
+  return (
+    <Fragment>
+      <h2>Symmary and quantities</h2>
+    </Fragment>
+  );
+};
+
+export default Summary;
