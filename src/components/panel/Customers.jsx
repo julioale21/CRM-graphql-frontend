e2cb4a8f3a0ad96e2b6ee550d420a718b7895970
Julio Romero <julio_ale21@hotmail.com>
2021-03-28 19:03:26 -0300

    chore: added query to get top customers

--- src/components/panel/Customers.jsx
+++ src/components/panel/Customers.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { GET_TOP_CUSTOMERS } from "../../graphql/queries";
 
 const Customers = () => {
-  return <h1>Customer</h1>;
+  return (
+    <Query query={GET_TOP_CUSTOMERS} pollInterval={500}>
+      {({ loading, error, data, startPolling, stopPolling }) => {
+        if (loading) return "Loading...";
+        if (error) return `Error: ${error.message}`;
+        // eslint-disable-next-line no-console
+        console.log(data);
+        return <h1>Customer</h1>;
+      }}
+    </Query>
+  );
 };
 
 export default Customers;
