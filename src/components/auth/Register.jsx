7742bc8c40e04ef5fa480f12451a6285965a5f18
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-03-29 12:36:37 -0300

    styles: added styles to register form

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,7 +1,41 @@
 import React, { Component, Fragment } from "react";
 
+const initialState = {
+  username: "",
+  password: "",
+  repeatedPassword: "",
+  showPassword: false,
+  showRepeatedPassword: false,
+};
 class Register extends Component {
-  state = {};
+  state = {
+    ...initialState,
+  };
+
+  updateState = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleIconPassword = () => {
+    this.setState({
+      showPassword: !this.state.showPassword,
+    });
+  };
+
+  handleIconRepeatedPassword = () => {
+    this.setState({
+      showRepeatedPassword: !this.state.showRepeatedPassword,
+    });
+  };
+
+  validateForm = () => {
+    const { username, password, repeatedPassword } = this.state;
+    return username !== "" && password !== "" && password === repeatedPassword;
+  };
+
   render() {
     return (
       <Fragment>
@@ -15,6 +49,7 @@ class Register extends Component {
               </label>
               <div className="flex w-full border border-4 border-gray-300 rounded-md p-1 focus-within:ring focus-within:ring-green-200 focus-within:border-none">
                 <input
+                  onChange={this.updateState}
                   type="text"
                   name="username"
                   placeholder="Name of user"
@@ -32,13 +67,20 @@ class Register extends Component {
               </label>
               <div className="flex w-full border border-4 border-gray-300 rounded-md p-1 focus-within:ring focus-within:ring-green-200 focus-within:border-none">
                 <input
-                  type="password"
+                  onChange={this.updateState}
+                  type={`${this.state.showPassword ? "text" : "password"}`}
                   name="password"
                   placeholder="Enter your password"
                   className="outline-none py-1 pl-3 w-full placeholder-gray-500 placeholder-opacity-50"
                 />
                 <span className="flex items-center leading-normal bg-white px-3 border-0 rounded rounded-r-none text-2xl text-gray-600">
-                  <i className="fas fa-lock fill-current text-green-400"></i>
+                  <i
+                    onClick={this.handleIconPassword}
+                    role="button"
+                    className={`fas fa-sm ${
+                      this.state.showPassword ? "fa-eye-slash" : "fa-eye"
+                    } fill-current text-green-400`}
+                  ></i>
                 </span>
               </div>
             </div>
@@ -46,24 +88,37 @@ class Register extends Component {
             <div className="mt-3">
               <label
                 className="font-bold text-green-500 text-sm"
-                htmlFor="repeat-passwor"
+                htmlFor="repeatedPassword"
               >
                 Repeat Password
               </label>
               <div className="flex w-full border border-4 border-gray-300 rounded-md p-1 focus-within:ring focus-within:ring-green-200 focus-within:border-none">
                 <input
-                  type="password"
-                  name="repeat-password"
+                  onChange={this.updateState}
+                  type={`${this.state.showRepeatedPassword ? "text" : "password"}`}
+                  name="repeatedPassword"
                   placeholder="Enter your password"
                   className="outline-none py-1 pl-3 w-full placeholder-gray-500 placeholder-opacity-50"
                 />
                 <span className="flex items-center leading-normal bg-white px-3 border-0 rounded rounded-r-none text-2xl text-gray-600">
-                  <i className="fas fa-lock fill-current text-green-400"></i>
+                  <i
+                    onClick={this.handleIconRepeatedPassword}
+                    role="button"
+                    className={`fas fa-sm ${
+                      this.state.showRepeatedPassword ? "fa-eye-slash" : "fa-eye"
+                    } fill-current text-green-400`}
+                  ></i>
                 </span>
               </div>
             </div>
 
-            <button className="float-right focus:outline-none btn border border-green-400 hover:bg-green-200 hover:border-green-200 mt-5 text-sm sm:text-base">
+            <button
+              className={`${
+                this.validateForm()
+                  ? "focus:outline-none hover:bg-green-200 border-green-400 hover:border-green-200"
+                  : "btn-disabled bg-gray-100 border-gray-100"
+              } btn border float-right text-sm sm:text-base mt-5`}
+            >
               Register
             </button>
           </div>
