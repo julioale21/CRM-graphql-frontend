8286c4ea72045ff680e4683bc6b240a006581164
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-03-30 11:35:21 -0300

    chore: added send token to backend

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+  request: (operation) => {
+    const token = localStorage.getItem("token");
+    operation.setContext({
+      headers: {
+        authorization: token,
+      },
+    });
+  },
   cache: new InMemoryCache({
     addTypename: false,
   }),
